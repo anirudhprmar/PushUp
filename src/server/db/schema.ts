eb0440adcc0512f4cfa6c99b5892df6f52cba2f8
Anirudh Parmar <anirudhparmar2004@gmail.com>
2025-11-04 22:02:22 +0530

    inserted temp data

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,19 @@
 import { pgTable, serial, text, timestamp, index,boolean } from "drizzle-orm/pg-core"
 
+export const posts = pgTable(
+  "posts",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  },
+  (table) => [
+    index("Post_name_idx").on(table.name)
+  ]
+)
+
+
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
